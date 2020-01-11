62be9e32e6da5f3c0fdccbd630a22a7a95df4d6f
He Biao <47956618+zhukexingkong@users.noreply.github.com>
2020-01-11 17:11:01 +0800

    SpringBoot+MyBatis

---
+++ day01_springboot_mybatis/src/main/java/com/learn/Day01SpringbootMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01SpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day01SpringbootMybatisApplication.class, args);
+    }
+
+}

abe3658a6996a8308cb88e49cee42f55f96bf9ba
HeBiao <875973218@qq.com>
2020-01-11 17:57:32 +0800

    修改配置

--- day01_springboot_mybatis/src/main/java/com/learn/SpringbootMybatisApplication.java
+++ day01_springboot_mybatis/src/main/java/com/learn/SpringbootMybatisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Day01SpringbootMybatisApplication {
+public class SpringbootMybatisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Day01SpringbootMybatisApplication.class, args);
+        SpringApplication.run(SpringbootMybatisApplication.class, args);
     }
 
 }

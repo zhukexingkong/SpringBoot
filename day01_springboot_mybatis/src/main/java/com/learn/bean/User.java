e656d797541525f5c2ccb58b24d1b63d02d605ea
HeBiao <875973218@qq.com>
2020-01-13 16:43:13 +0800

    添加lombok依赖

--- day01_springboot_mybatis/src/main/java/com/learn/bean/User.java
+++ day01_springboot_mybatis/src/main/java/com/learn/bean/User.java
@@ -1,43 +1,13 @@
 package com.learn.bean;
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class User {
     private int id;
     private String userName;
     private String password;
     private String name;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- day01_springboot_mybatis/src/main/java/com/learn/utils/TimerUtil.java
+++ day01_springboot_mybatis/src/main/java/com/learn/utils/TimerUtil.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Component
 public class TimerUtil {
-    @Scheduled(initialDelay = 1000,fixedRate = 1000)
+    //@Scheduled(initialDelay = 1000,fixedRate = 1000)
     public void myTask(){
         System.out.println("当前系统时间: " + LocalDateTime.now());
     }

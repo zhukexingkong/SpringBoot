ad391ee663b77c95b7c2e6eccc791bd8b39bcd30
HeBiao <875973218@qq.com>
2020-01-13 15:17:34 +0800

    添加Redis依赖; 添加定时器

--- day01_springboot_mybatis/src/main/java/com/learn/controller/HelloController.java
+++ day01_springboot_mybatis/src/main/java/com/learn/controller/HelloController.java
@@ -1,7 +1,7 @@
 package com.learn.controller;
 
 import com.learn.bean.User;
-import com.learn.Mappers.UserMapper;
+import com.learn.mappers.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- day01_springboot_mybatis/src/test/java/com/learn/Day01SpringbootMybatisApplicationTests.java
+++ day01_springboot_mybatis/src/test/java/com/learn/Day01SpringbootMybatisApplicationTests.java
@@ -1,13 +1,39 @@
 package com.learn;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learn.mappers.UserMapper;
+import com.learn.bean.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @SpringBootTest
 class Day01SpringbootMybatisApplicationTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
     }
-
+    @Test
+    public void testRedis() throws JsonProcessingException {
+        String users = (String) redisTemplate.boundValueOps("user.findAll").get();
+        if (StringUtils.isEmpty(users)) {
+            List<User> userList = userMapper.findAll();
+            ObjectMapper jsonFormat = new ObjectMapper();
+            users = jsonFormat.writeValueAsString(userList);
+            redisTemplate.boundValueOps("user.findAll").set(users);
+            System.out.println("==============从数据库中获取用户数据 ===================");
+        }else {
+            System.out.println("==============从Redis缓存中获取用户数据 ===================");
+        }
+        System.out.println(users);
+    }
 }
